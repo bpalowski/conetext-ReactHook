e09be8894abb2d0f0c1bde2a748e53a97133a480
Brian Palowski <brianpalowski@techtonicadmins-MacBook-Pro.local>
2019-09-26 08:33:31 -0600

    Changed Booklist to use context with react hooks

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -1,30 +1,21 @@
-import React, { Component } from 'react';
+import React, { useContext } from 'react';
 import { ThemeContext } from '../contexts/ThemeContext';
 
-class BookList extends Component {
-  // static contextType = ThemeContext;
-  render() {
-    return (
-      <ThemeContext.Consumer>
-        {context => {
-          const { isLight, light, dark } = context;
-          const theme = isLight ? light : dark;
-          return (
-            <div
-              className="book-list"
-              style={{ color: theme.syntax, background: theme.bg }}
-            >
-              <ul>
-                <li style={{ background: theme.ui }}>the way of kings</li>
-                <li style={{ background: theme.ui }}>the other way</li>
-                <li style={{ background: theme.ui }}>the final way </li>
-              </ul>
-            </div>
-          );
-        }}
-      </ThemeContext.Consumer>
-    );
-  }
-}
+const BookList = () => {
+  const { isLight, light, dark } = useContext(ThemeContext);
+  const theme = isLight ? light : dark;
+  return (
+    <div
+      className="book-list"
+      style={{ color: theme.syntax, background: theme.bg }}
+    >
+      <ul>
+        <li style={{ background: theme.ui }}>the way of kings</li>
+        <li style={{ background: theme.ui }}>the other way</li>
+        <li style={{ background: theme.ui }}>the final way </li>
+      </ul>
+    </div>
+  );
+};
 
 export default BookList;
