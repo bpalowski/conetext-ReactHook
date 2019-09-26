81c8d3c7087a413500226631a30bbb32620d634d
Brian Palowski <brianpalowski@techtonicadmins-MacBook-Pro.local>
2019-09-26 09:03:09 -0600

    Updated with contex and hook

--- src/App.js
+++ src/App.js
@@ -4,13 +4,16 @@ import BookList from './components/BookList.jsx';
 import ThemeContextProvider from './contexts/ThemeContext';
 import AuthContextProvider from './contexts/AuthContext';
 import ThemeToggle from './components/ThemeToggle';
+import BookContextProvider from './contexts/BookContext';
 function App() {
   return (
     <div className="App">
       <AuthContextProvider>
         <ThemeContextProvider>
           <Navbar />
-          <BookList />
+          <BookContextProvider>
+            <BookList />
+          </BookContextProvider>
           <ThemeToggle />
         </ThemeContextProvider>
       </AuthContextProvider>
