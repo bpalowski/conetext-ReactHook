0e2a59d7b1d92eafb873db78730131a7d736f138
Brian Palowski <brianpalowski@techtondminsMBP.localdomain>
2019-09-26 11:30:46 -0600

    Updated to hooks pt2

---
+++ src/contexts/BookContext.js
@@ -0,0 +1,24 @@
+import React, { createContext, useState } from 'react';
+import uuid from 'uuid/v1';
+
+export const BookContext = createContext();
+
+const BookContextProvider = props => {
+  const [books, setBooks] = useState([
+    { title: 'name of the wind', author: 'pman', id: 1 },
+    { title: 'name of the tree', author: 'yess', id: 2 }
+  ]);
+  const addBook = (title, author) => {
+    setBooks([...books, { title: title, author: author, id: uuid() }]);
+  };
+  const removeBook = id => {
+    setBooks(books.filter(book => book.id !== id));
+  };
+  return (
+    <BookContext.Provider value={{ books, addBook, removeBook }}>
+      {props.children}
+    </BookContext.Provider>
+  );
+};
+
+export default BookContextProvider;
